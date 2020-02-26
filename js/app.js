af90d0699e75a35517163ebfc87e72323eb8f6ee
Enjoystic <enjoystic@Mac-Mini-Joss.local>
2020-02-25 18:09:45 -0600

    version mobile

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+
+
+$('.home_s').on('click', function(e) {
+    e.preventDefault();
+    var strAncla = '#' + $(this).data('home');
+    $('html,body').animate({ scrollTop: $(strAncla).offset().top }, 2000);
+});
+$('.services_s').on('click', function(e) {
+    e.preventDefault();
+    var strAncla = '#' + $(this).data('services');
+    $('html,body').animate({ scrollTop: $(strAncla).offset().top }, 2000);
+});
+$('.work_s').on('click', function(e) {
+    e.preventDefault();
+    var strAncla = '#' + $(this).data('work');
+    $('html,body').animate({ scrollTop: $(strAncla).offset().top }, 2000);
+});
+$('.projets_s').on('click', function(e) {
+    e.preventDefault();
+    var strAncla = '#' + $(this).data('projets');
+    $('html,body').animate({ scrollTop: $(strAncla).offset().top }, 2000);
+});
